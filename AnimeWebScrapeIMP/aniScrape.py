45bc9feff3a72b60701e1bad5e159dc501206246
Just_Ajani <ajanicox248@gmail.com>
2024-09-05 11:00:28 -0500

    Update aniScrape.py
    Refactoring almost everything

--- AnimeWebScrapeIMP/aniScrape.py
+++ AnimeWebScrapeIMP/aniScrape.py
@@ -10,62 +10,68 @@ import pandas as pd
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
 import base64
-from tqdm import tqdm  # tqdm is a library that provides a progress meter
 import sys
 import os
 
 class AniScrape:
-    def __init__(self, animeName):
-        self.animeName = animeName
+    def __init__(self):
+        self.animeName = input("Enter Anime Name: ")
         self.animUrl = None 
         self.episodeId = None
+        self.episodeNum = int(input("Enter Episode Number: "))
+        self.command = input("Type 'enable' to enable auto skip intro and outro or 'disable' to disable features: ").lower()
         service = Service(ChromeDriverManager().install())
         adblocker = Options()
-        adblocker.add_extension(f'C:\\Users\\ajani\Downloads\\MLOMIEJDFKOLICHCFLEJCLCBMPEANIIJ_10_4_3_0.crx')
+        adblocker.add_extension(r'C:\\Users\\ajani\\Downloads\\MLOMIEJDFKOLICHCFLEJCLCBMPEANIIJ_10_4_3_0.crx')  # Corrected path string with raw string literal
         self.driver = webdriver.Chrome(service=service, options=adblocker)
+        self.driver.set_script_timeout(60)
         
-    def saveAnimeList(self, category):
-        AnimFound = []
-        link = []
-        AnimFound.append(category.text)
-        link.append(category.get_attribute('href'))
-        df = pd.DataFrame({'Anime Found': AnimFound, 'Link': link})
-        df.to_csv('AnimeList.csv', index=False)
+    # def saveAnimeList(self, category):
+    #     AnimFound = []
+    #     link = []
+    #     AnimFound.append(category.text)
+    #     link.append(category.get_attribute('href'))
+    #     df = pd.DataFrame({'Anime Found': AnimFound, 'Link': link})
+    #     df.to_csv('AnimeList.csv', index=False)
 
     def searchAnime(self):
         self.driver.get('https://9animetv.to/home')
-        search = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//input[@name="keyword"]'))
-        )
-        search.send_keys(self.animeName + Keys.ENTER)
-    
+        try:
+            search = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//input[@name="keyword"]'))
+            )
+            search.send_keys(self.animeName + Keys.ENTER)
+        except TimeoutException:
+            print("Search bar not found on the page.")
+
     def clickAnimeFromList(self):
-        self.driver.get(f'https://9animetv.to/search?keyword={self.animeName.replace(" ", "+")}')
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, '//h3[@class="film-name"]/a'))
-        )
-        categories = self.driver.find_elements(By.XPATH, '//h3[@class="film-name"]/a')
-        for category in categories:
-            if self.animeName.lower() == category.text.lower():
-                self.driver.execute_script("arguments[0].scrollIntoView(true);", category)
-                WebDriverWait(self.driver, 10).until(
-                    EC.element_to_be_clickable(category)
-                )
-                self.animUrl = category.get_attribute('href')  
-                print(f"Captured URL: {self.animUrl}")  
-                break
+        self.driver.get(self.driver.current_url)
+        try:
+            categories = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//h3[@class="film-name"]/a'))
+            )
+            for category in categories:
+                if self.animeName.lower() == category.text.lower():
+                    self.driver.execute_script("arguments[0].scrollIntoView(true);", category)
+                    WebDriverWait(self.driver, 5).until(
+                        EC.element_to_be_clickable(category)
+                    )
+                    self.animUrl = category.get_attribute('href')  
+                    print(f"Captured URL: {self.animUrl}")  
+                    break
+        except TimeoutException:
+            print("Anime list not loaded or no matching anime found.")
     
-    def watchAnime(self, Epnum):
+    def watchAnime(self):
         if self.animUrl:
             try:
                 print(f"Navigating to URL: {self.animUrl}")
                 self.driver.get(self.animUrl)
-                WebDriverWait(self.driver, 20).until(
+                episodes = WebDriverWait(self.driver, 10).until(
                     EC.presence_of_all_elements_located((By.XPATH, '//a[@data-id]'))
                 )
-                episodes = self.driver.find_elements(By.XPATH, '//a[@data-id]')
                 baseId = int(episodes[0].get_attribute('data-id'))
-                targetId = baseId + (Epnum - 1)
+                targetId = baseId + (self.episodeNum - 1)
 
                 foundEpisode = False
                 for episode in episodes:
@@ -74,11 +80,6 @@ class AniScrape:
                         foundEpisode = True
                         episodeUrl = f"{self.animUrl}?ep={targetId}"
                         print(f"Navigating directly to episode URL: {episodeUrl}")
-                        self.driver.get(episodeUrl)
-                        WebDriverWait(self.driver, 10).until(
-                            EC.presence_of_element_located((By.XPATH, "//div[@class='video-player']"))
-                        )
-                        print("Episode loaded successfully.")
                         break
 
                 if not foundEpisode:
@@ -87,39 +88,49 @@ class AniScrape:
                         self.driver.execute_script("arguments[0].scrollIntoView(true);", range_)
                         episodes = range_.find_elements(By.XPATH, './/a[@data-id]')
                         for episode in episodes:
-                            if self.episodeId == targetId:
+                            if int(episode.get_attribute('data-id')) == targetId:
                                 foundEpisode = True
                                 episodeUrl = f"{self.animUrl}?ep={targetId}"
                                 print(f"Navigating directly to episode URL: {episodeUrl}")
-                                self.driver.get(episodeUrl)
-                                WebDriverWait(self.driver, 10).until(
-                                    EC.presence_of_element_located((By.XPATH, "//div[@class='video-player']"))
-                                )
-                                print("Episode loaded successfully.")
                                 break
-                elif targetId != targetId:
-                    print(f"No episode found with data-id {targetId}, loading episode 1 instead.")
+                
+                if not foundEpisode:
                     firstEpisodeId = int(episodes[0].get_attribute('data-id'))
-                    firstEpisodeUrl = f"{self.animUrl}?ep={firstEpisodeId}"
-                    print(f"Navigating directly to episode URL: {firstEpisodeUrl}")
-                    self.driver.get(firstEpisodeUrl)
-                    WebDriverWait(self.driver, 10).until(
-                        EC.presence_of_element_located((By.XPATH, "//div[@class='video-player']"))
-                    )
-                    print("First episode loaded successfully.")
+                    episodeUrl = f"{self.animUrl}?ep={firstEpisodeId}"
+                    print(f"No episode found with ID {targetId}, loading episode 1 instead: {episodeUrl}")
+ 
+                self.driver.get(episodeUrl)
+                self.downloadBTN()
+                self.manageDownloads()
+
             except Exception as e:
                 print(f"An error occurred during episode navigation: {e}")
+            finally:
+                self.quit()
 
-              
-            self.skipIntro()
-            self.nextEpisode(self.episodeId)
-            self.quit()
-    
     def handleIframe(self):
-        if 'iframe-embed' not in self.driver.current_url:
-                        WebDriverWait(self.driver, 5).until(
-                            EC.frame_to_be_available_and_switch_to_it((By.ID, 'iframe-embed'))
-                        )
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.frame_to_be_available_and_switch_to_it((By.ID, 'iframe-embed'))
+            )
+        except TimeoutException:
+            print("Failed to load iframe.")
+    
+    def enableFeatures(self): 
+        if self.command == 'enable':
+            try:
+                self.skipIntro()
+                self.nextEpisode()
+                print("Features enabled.")
+                return True
+            except Exception as e:
+                print(f"Failed to enable features: {e}")
+        elif self.command == 'disable':
+            print("Features disabled.")
+            return False
+        else:
+            print("Invalid command.")
+            return False
 
     def skipIntro(self):
         try:
@@ -129,31 +140,16 @@ class AniScrape:
             while not introSkipped and (time.time() - start_time) < 300:
                 try:
                     self.handleIframe()
-
-                    timeText = self.driver.execute_script("""
-                        var elem = document.querySelector('#vidcloud-player > div.jw-wrapper.jw-reset > div.jw-controls.jw-reset > div.jw-controlbar.jw-reset > div.jw-reset.jw-button-container > div.jw-icon.jw-icon-inline.jw-text.jw-reset.jw-text-elapsed');
-                        return elem ? elem.textContent : 'not found';
-                    """)
-
-
-                    if timeText == 'not found':
-                        print("Time element not found, retrying...")
-                        time.sleep(1)
-                        continue
-
-                    if ':' in timeText:
-                        minutes, seconds = timeText.split(':')
-                        elapsedSeconds = int(minutes) * 60 + int(seconds)
-                        print(f"Current video time: {elapsedSeconds} seconds")
-                    else:
-                        print("Time text not in expected format:", timeText)
-                        continue
-
-                    if elapsedSeconds <= 600:
-                        self.driver.execute_script("var btn = document.querySelector('#skip-intro'); if (btn) btn.click();")
+                    
+                    SkipBTN = WebDriverWait(self.driver, 10).until(
+                            EC.element_to_be_clickable((By.ID, 'skip-intro'))
+                        )
+                        
+                    if SkipBTN.is_displayed and SkipBTN.is_enabled:
+                        SkipBTN.click()
                         print("Intro skipped successfully.")
                         introSkipped = True
-                    
+
                     self.driver.switch_to.default_content()
 
                 except Exception as e:
@@ -167,34 +163,22 @@ class AniScrape:
         except Exception as e:
             print(f"General error during intro skipping: {e}")
 
-    def nextEpisode(self, Epnum):  
+    def nextEpisode(self):  
         try:
             nextEp = False
             startTime = time.time()
             while not nextEp and (time.time() - startTime) < 1380:
                 try:
                     self.handleIframe()
-                    timeText = self.driver.execute_script("""
-                        var elem = document.querySelector('#vidcloud-player > div.jw-wrapper.jw-reset > div.jw-controls.jw-reset > div.jw-controlbar.jw-reset > div.jw-reset.jw-button-container > div.jw-icon.jw-icon-inline.jw-text.jw-reset.jw-text-countdown');
-                        return elem ? elem.textContent : 'not found';
-                    """)
+        
+                    nextEp = WebDriverWait(self.driver, 10).until(
+                        EC.element_to_be_clickable((By.ID, 'skip-outro'))
+                    )
 
-                    if timeText == 'not found':
-                        print("Time element not found, retrying...")
-                        time.sleep(1)
-                        continue
-
-                    if ':' in timeText:
-                        minutes, seconds = timeText.split(':')
-                        countDownSeconds = int(minutes) * 60 + int(seconds)
-                    else:
-                        print("Time text not in expected format:", timeText)
-                        continue
-
-                    if countDownSeconds <= 120:
-                        self.driver.execute_script("var btn = document.querySelector('#next-episode'); if (btn) btn.click();")
-                        episodeUrl = f"{self.animUrl}?ep={Epnum + 1}"
-                        self.driver.get(episodeUrl)
+                    if nextEp.is_displayed and nextEp.is_enabled:
+                        nextEp.click()
+                        skippedEp = f"{self.animUrl}?ep={self.episodeNum + 1}"
+                        self.driver.get(skippedEp)
                         print("Next episode loaded successfully.")
                         nextEp = True 
 
@@ -210,51 +194,92 @@ class AniScrape:
         except ValueError:
             print("Error processing countdown timer value.")
         self.quit()
+
+    def manageDownloads(self):
+        pathFolder = r'C:\\Users\\ajani\\Downloads\\animeScrape'
+        while True:  
+            if self.isBTNClick():
+                self.downloadEpisode(pathFolder)
+            time.sleep(1)
     
-    def ensureDirectoryExists(self,path):
+    def ensureDirectoryExists(self, path):
         directory = os.path.dirname(path)
+        print(f"Checking if directory exists: {directory}")  
         if not os.path.exists(directory):
+            print(f"Directory does not exist, creating: {directory}")  
             os.makedirs(directory)
+        else:
+            print(f"Directory already exists: {directory}")  
+        
+    def downloadBTN(self):
+        dlBTN = """
+                var dlButton = document.createElement('button');
+                dlButton.innerText = 'Download Episode';
+                dlButton.style.position = 'fixed';
+                dlButton.style.top = '10px';
+                dlButton.style.right = '10px';
+                dlButton.onclick = function() { localStorage.setItem('downloadClicked', 'true'); };
+                document.body.appendChild(dlButton);
+                """
+        self.driver.execute_script(dlBTN)
     
-    def downLoadEpisode(self, destination):
+    def isBTNClick(self):
+        downloadClicked = self.driver.execute_script("return localStorage.getItem('downloadClicked') === 'true';")
+        if downloadClicked:
+            self.driver.execute_script("localStorage.removeItem('downloadClicked');")  
+            return True
+        return False
+
+    def downloadEpisode(self, destination):
         try:
             self.ensureDirectoryExists(destination)
             self.handleIframe()
-            videoSrc = self.driver.execute_script("""
-                    var video = document.querySelector('#vidcloud-player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video')
-                    return video ? video.src : 'not found'""")
-            print(f"Video source URL: {videoSrc}")
-
-            if videoSrc == 'not found':
-                print("Video source not found.")
-                return
-            if videoSrc.startswith('blob:'):
-                dataUrl = self.driver.execute_script("""
-                    var blobUrl = arguments[0];
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', blobUrl, true);
-                    xhr.responseType = 'blob';
-                    xhr.onload = function() {
-                        var reader = new FileReader();
-                        reader.onload = function(e) {
-                            window.dataURL = reader.result;
-                        };
-                        reader.readAsDataURL(xhr.response);
-                    };
-                    xhr.send();
-                    return 'dataURL ready';
-                """, videoSrc)
-                time.sleep(10)
-                dataUrl = self.driver.execute_script("return window.dataURL;")
-            else:
-                dataUrl = videoSrc
             
-            if dataUrl.startswith('data:'):
-                contentType,base64Data = dataUrl.split(';',1)
-                videoData = base64.b64decode(base64Data)
-                fileExtension =contentType.split('/')[1].split(';')[0]
+            timeout = time.time() + 60 * 5  # Wait for up to 5 minutes.
+            videoSrc = None
 
-            # Write video data to file
+            while time.time() < timeout:
+                videoSrc = self.driver.execute_script("""
+                    var video = document.querySelector('#vidcloud-player > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video');
+                    return video ? video.src : null;
+                """)
+                if videoSrc and videoSrc.startswith('blob:'):
+                    print(f"Blob URL found: {videoSrc}")
+                    break
+                print("Waiting for video source...")
+                time.sleep(5)  
+
+            if not videoSrc:
+                print("Video source URL not found or not valid.")
+                return
+
+            print("Handling blob URL for video.")
+            dataUrl = self.driver.execute_async_script("""
+                var blobUrl = arguments[0], callback = arguments[arguments.length - 1];
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', blobUrl, true);
+                xhr.responseType = 'blob';
+                xhr.onload = function() {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        callback(reader.result);  
+                    };
+                    reader.onerror = function() {
+                        callback(null);
+                    };
+                    reader.readAsDataURL(xhr.response);
+                };
+                xhr.onerror = xhr.onabort = function() {
+                    callback(null);  
+                };
+                xhr.send();
+            """, videoSrc)
+
+            if dataUrl and dataUrl.startswith('data:'):
+                contentType, base64Data = dataUrl.split(';', 1)
+                videoData = base64.b64decode(base64Data.split(',')[1])
+                fileExtension = contentType.split('/')[1].split(';')[0]
+
                 filePath = os.path.join(destination, f"downloaded_video.{fileExtension}")
                 with open(filePath, 'wb') as file:
                     file.write(videoData)
@@ -264,13 +289,11 @@ class AniScrape:
         except Exception as e:
             print(f"An error occurred while trying to download the episode: {e}")
 
-
-    def run(self, Epnum):
+    def run(self):
         self.searchAnime()
         self.clickAnimeFromList()
-        self.watchAnime(Epnum)
-        self.skipIntro()
-        self.nextEpisode(self.episodeId)
+        self.watchAnime()
+        self.enableFeatures()
         return self.driver
 
     def quit(self):
@@ -278,6 +301,8 @@ class AniScrape:
         self.driver.quit()
         sys.exit()
 
-animInput = input("Enter Anime Name: ")
-episodeNum = int(input("Enter Episode Number: "))
-Anime = AniScrape(animInput).run(episodeNum)
+AniScrape().run()
+
+
+
+
